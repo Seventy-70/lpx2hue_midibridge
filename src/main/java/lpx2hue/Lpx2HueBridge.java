754ae58d13c274f11dafa9292931614a9086f71e
nijhora1 <rnijholt@gmail.com>
2016-06-16 18:57:00 +0200

    introduced MidiDeviceProxy

--- src/main/java/lpx2hue/Lpx2HueBridge.java
+++ src/main/java/lpx2hue/Lpx2HueBridge.java
@@ -1,8 +1,10 @@
 package lpx2hue;
 
 
+import com.sun.media.sound.MidiOutDeviceProvider;
 import lpx2hue.beans.MidiSettings;
 import lpx2hue.handlers.Lpx2HueEventHandler;
+import lpx2hue.iodevice.MidiDeviceProxy;
 import lpx2hue.iodevice.MidiInput;
 import lpx2hue.iodevice.MidiInputDevice;
 import lpx2hue.iodevice.MidiOutputDevice;
@@ -27,6 +29,7 @@ import static java.lang.Thread.sleep;
  * You can ask Lpx2HueMain about available input and output devices. You can then use the methods
  * on the returned objects to open an input or output midi port.
  *
+ * written by RJN
  */
 
 @Configuration
@@ -34,121 +37,17 @@ import static java.lang.Thread.sleep;
 @ComponentScan
 public class Lpx2HueBridge {
 
-    /**
-	 *
-	 * @return the list of the available input devices.
-	 */
-	public static MidiInputDevice[] getInputDevices() {
-		javax.sound.midi.MidiDevice.Info infos[] = MidiSystem.getMidiDeviceInfo();
-		ArrayList<MidiInputDevice> result = new ArrayList<MidiInputDevice>();
-		for (javax.sound.midi.MidiDevice.Info info : infos) {
-			javax.sound.midi.MidiDevice device;
-			try {
-				device = MidiSystem.getMidiDevice(info);
-				if (device.getMaxTransmitters() == 0)
-					continue;
-				result.add(new MidiInputDevice(info));
-			} catch (MidiUnavailableException e) {
-				e.printStackTrace();
-				continue;
-			}
-		}
-		return result.toArray(new MidiInputDevice[0]);
-	}
-
-	/**
-	 *
-	 * @return the list of the available output devices.
-	 */
-	public static MidiOutputDevice[] getOutputDevices() {
-		javax.sound.midi.MidiDevice.Info infos[] = MidiSystem.getMidiDeviceInfo();
-		ArrayList<MidiOutputDevice> result = new ArrayList<MidiOutputDevice>();
-		for (javax.sound.midi.MidiDevice.Info info : infos) {
-			javax.sound.midi.MidiDevice device;
-			try {
-				device = MidiSystem.getMidiDevice(info);
-				if (device.getMaxReceivers() == 0)
-					continue;
-				result.add(new MidiOutputDevice(info));
-			} catch (MidiUnavailableException e) {
-				e.printStackTrace();
-				continue;
-			}
-		}
-		return result.toArray(new MidiOutputDevice[0]);
-	}
-
-	/**
-	 *
-	 * @return a list of the output devices names
-	 */
-	public static String[] getOutputDeviceNames() {
-		MidiOutputDevice[] devices = getOutputDevices();
-		ArrayList<String> result = new ArrayList<String>();
-		for (MidiOutputDevice device : devices) {
-			result.add(device.getName());
-		}
-		return result.toArray(new String[0]);
-	}
-
-	/**
-	 * Returns a specific output device
-	 * @param name the name of the output device
-	 * @return
-	 */
-	public static MidiOutputDevice getOutputDevice(String name) {
-		MidiOutputDevice[] devices = getOutputDevices();
-		for (MidiOutputDevice device : devices) {
-			if (name.equals(device.getName()))
-				return device;
-		}
-		return null;
-	}
-
-	/**
-	 *
-	 * @return a list of the output devices names
-	 */
-	public static String[] getInputDeviceNames() {
-		MidiInputDevice[] devices = getInputDevices();
-		ArrayList<String> result = new ArrayList<String>();
-		for (MidiInputDevice device : devices) {
-			result.add(device.getName());
-		}
-		return result.toArray(new String[0]);
-	}
-
-	/**
-	 * Returns a specific input device
-	 * @param name the name of the input device
-	 * @return
-	 */
-	public static MidiInputDevice getInputDevice(String name) {
-		MidiInputDevice[] devices = getInputDevices();
-		for (MidiInputDevice device : devices) {
-			if (name.equals(device.getName()))
-				return device;
-		}
-		return null;
-	}
-
 	public static void main(String args[]) throws InterruptedException {
 		System.setProperty("apple.awt.UIElement", "true");
         System.out.println("LPX2HUE Midi Bridge startup...");
-        System.out.println("List of all MidiSystem Input & Output Devices:");
+        System.out.println("List of all MidiSystem input devices:");
 
 		ApplicationContext context = new AnnotationConfigApplicationContext(Lpx2HueBridge.class);
-//		final ApplicationContext context = new ClassPathXmlApplicationContext("application.properties");
-		MidiSettings ms = context.getBean(MidiSettings.class);
-		int i = 0;
-        MidiInputDevice selectedInputDevice = null;
+		MidiSettings midiSettings = context.getBean(MidiSettings.class);
 
-		for (MidiInputDevice device : getInputDevices()) {
-			System.out.println("- input device " + i + ": "  + device);
-			if (device.getName().contains(ms.getInput()))
-				 selectedInputDevice = device;
-			i++;
-		}
+		MidiDeviceProxy proxy = new MidiDeviceProxy();
+		proxy.printInputDevices();
+		MidiInputDevice selectedInputDevice = proxy.getInputDevice(midiSettings.getInput());
 
         System.out.println("Setting up midi event handler for LPX2HUE Midi Bridge...");
         if (selectedInputDevice!=null){
@@ -163,7 +62,7 @@ public class Lpx2HueBridge {
             }
 
         } else {
-            System.out.println("Failed to connect to midi input device with name: "+ ms.getInput()+". Please check your Midi Studio IAC driver setup." );
+            System.out.println("Failed to connect to midi input device with name: "+ midiSettings.getInput()+". Please check your Midi Studio IAC driver setup." );
             exit(0);
         }
 	}
